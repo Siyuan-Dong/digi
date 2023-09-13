d1e9d083fb23b41924a564fe5c1bd5e0f56a103e
Siyuan-Dong <dongsiyuan@pku.edu.cn>
2023-09-13 08:40:10 +0800

    remove examples and mocks

--- examples/digiphone/driver/handler.py
+++
@@ -1,9 +0,0 @@
-import digi
-
-@digi.on.model
-def h(model):
-    ...
-
-if __name__ == '__main__':
-    digi.run()
-    digi.message.start_listening()
--- examples/queries/timescaledb/lamp_brightness.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE l1 (
-    ts TIMESTAMP PRIMARY KEY,
-    brightness FLOAT
-);
-
--- SELECT brightness, time_bucket(INTERVAL '1 second', ts) AS bucket FROM l1;
